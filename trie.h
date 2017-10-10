b8de1fbbf99daea158c21e79de83cc41f9c59f4b
Gdask <gdasky@gmail.com>
2017-10-10 20:39:25 +0300

    First attemp to declare classes

--- trie.h
+++ trie.h
@@ -1,7 +1,34 @@
 #ifndef TRIE_H
 #define	TRIE_H
 
+#include <string>
 
+using namespace std;
+
+class trie_node;
+
+//children has an array that contain pointers at the children of the node.
+//Children of that node are alphabeticly order in the array.
+//Array size starts with Init_Size and doubles everytime the array is full.
+//Hint: Use malloc & realloc!
+class children{
+	int Size;
+	int Current;
+	trie_node **array;
+
+	void move_data();
+public:
+	children(int Init_Size);
+	~children();
+	void Insert_Child(trie_node* new_child);
+	trie_node* Search_Child(string keyword);
+}
+
+class trie_node {
+	string word;
+	bool final_node;
+	children tmp_name;
+}
 
 
 
