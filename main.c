a633b50aacae76eb009acf4036c097de96cc5074
eirini.kaklopoulou@gmail.com <eirini.kaklopoulou@gmail.com>
2017-10-20 14:52:39 +0300

    init_file can be handled now, give to fetch_line status (I OR Q)

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 #include "stdlib.h"
 #include "trie_node.h"
 #include "string_utils.h"
 #include "trie.h"
 
 int main(){
+    /*
     FILE* fp;
     fp=fopen("commands.txt","r");
     if(fp==NULL){
@@ -25,6 +27,27 @@ int main(){
 
     line_manager_fin(&lm);
     trie_fin(&tree);
+    fclose(fp);*/
+    
+    FILE *fp;
+    fp=fopen("commands.txt","rw");
+    line_manager lm;
+    line_manager_init(&lm, fp);
+    bool result=lm_fetch_line(&lm, 'I');
+    char* word;
+    while(result==true){
+        bool result_=lm_fetch_ngram(&lm);
+        while(result_==true){
+            word=lm_fetch_word(&lm);
+            while(word!=NULL){
+                printf("word return: %s\n",word);
+                word=lm_fetch_word(&lm);
+            }
+            result_=lm_fetch_ngram(&lm);
+        }
+        result=lm_fetch_line(&lm, 'I');
+    }
+    line_manager_fin(&lm);
     fclose(fp);
     return 0;
 }
