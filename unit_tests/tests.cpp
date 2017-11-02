7150234c340f8f273d620af778a7b9ab28e9d707
eirini.kaklopoulou@gmail.com <eirini.kaklopoulou@gmail.com>
2017-11-02 11:18:09 +0200

    Thinking about more test cases

--- unit_tests/tests.cpp
+++ unit_tests/tests.cpp
@@ -7,50 +7,103 @@
 #include <gtest/gtest.h>
 #include <string>
  
-/*
-TEST(CaseA, BigLine){
+
+TEST(Buffersize, BigLine){
     line_manager lm;
     FILE* in;
     in=fopen("input/BigLine", "r");
     line_manager_init(&lm, in, 'I');
-    ASSERT_EQ(true, lm_fetch_line(&lm));
+    EXPECT_EQ(true, lm_fetch_line(&lm));
     line_manager_fin(&lm);
     fclose(in);
-}*/
+}
 
 /*Asked for invalid task px delete as DELETE*/
-/*TEST(CaseB, InvalidTask){
+TEST(Errorhandling, InvalidTask){
     line_manager lm;
     FILE* in;
-    in=stdin;
-    printf("please give a query which is not started by A/Q/D, as DELETE bla bla \n");
+    in=fopen("input/InvalidTaskFile","r");
     line_manager_init(&lm, in, 'Q');
-    ASSERT_EQ(false,lm_fetch_line(&lm));
+    bool has_line;
+    has_line=lm_fetch_line(&lm);
+    while(has_line==true){
+        if(lm_is_insert(&lm)==true||lm_is_delete(&lm)==true||lm_is_query(&lm)==true){
+            EXPECT_EQ(true, has_line);
+        }
+        else{
+            EXPECT_EQ(false, has_line);
+        }
+        has_line=lm_fetch_line(&lm);
+    }
     line_manager_fin(&lm);
     fclose(in);
-}*/
+}
 
-/*TEST(CaseC, EmptyQuery){
+TEST(Emptyline, EmptyQuery){
     line_manager lm;
     FILE* in;
-    in=stdin;
-    printf("please give a query which starts by A/Q/D and nothing more \n");
+    in=fopen("input/EmptyQuery", "r");
     line_manager_init(&lm, in, 'Q');
-    ASSERT_EQ(false, lm_fetch_line(&lm));
+    EXPECT_EQ(false, lm_fetch_line(&lm));
     line_manager_fin(&lm);
     fclose(in);
-}*/
+}
 
-/*TEST(CaseC, EmptyNgrams){
+TEST(Emptyline, EmptyNgrams){
     line_manager lm;
     FILE* in;
-    in=stdin;
-    printf("please give a empty ngram(multiple spaces) \n");
+    in=fopen("input/EmptyNgram","r");
     line_manager_init(&lm, in, 'I');
-    ASSERT_EQ(false, lm_fetch_line(&lm));
+    EXPECT_EQ(false, lm_fetch_line(&lm));
     line_manager_fin(&lm);
     fclose(in);
-}*/
+}
+
+TEST(Spaces, MultipleSpaces){
+    line_manager lm;
+    FILE* in, *out;
+    in=fopen("input/MultipleSpaces", "r");
+    out=fopen("output/MultipleSpaces", "w+");
+    line_manager_init(&lm, in, 'Q');
+    trie db;
+    trie_init(&db,5);
+    result_manager rm;
+    result_manager_init(&rm, out);
+    bool has_line;
+    has_line=lm_fetch_line(&lm);
+    while(has_line==true){
+        if(lm_is_insert(&lm)==true){
+            lm_fetch_ngram(&lm);
+            trie_insert(&db,&lm);
+        }
+        else if(lm_is_delete(&lm)==true){
+            lm_fetch_ngram(&lm);
+            trie_delete(&db,&lm);
+        }
+        else if(lm_is_query(&lm)==true){
+            trie_search(&db,&lm,&rm);
+        }
+        else{
+            fprintf(stderr,"Corrupted line\n");
+        }
+        has_line=lm_fetch_line(&lm);
+    }
+
+    char buffer[100];
+    int size=100;
+    fseek(out, 0, SEEK_SET);
+    if(fgets(buffer, size, out)){
+        int r;
+        r=strcmp("s s|s s s\n", buffer);
+        EXPECT_EQ(0, r);
+    }
+
+    trie_fin(&db);
+    result_manager_fin(&rm);
+    line_manager_fin(&lm);
+    fclose(in);
+    fclose(out);
+}
 
 TEST(DeleteNgram, SearchAfter){
     line_manager lm;
@@ -99,22 +152,108 @@ TEST(DeleteNgram, SearchAfter){
 
 }
 
-/*
-TEST(DeleteNgramThatexistsMultipleTimes, SearchAfter){
 
+TEST(DeleteNgram, ExistMultipleTimes){
+    line_manager lm;
+    FILE* in, *out;
+    in=fopen("input/ExistMultipleTimes", "r");
+    out=fopen("output/ExistMultipleTimes", "w+");
+    line_manager_init(&lm, in, 'Q');
+    trie db;
+    trie_init(&db,5);
+    result_manager rm;
+    result_manager_init(&rm, out);
+    bool has_line;
+    has_line=lm_fetch_line(&lm);
+    while(has_line==true){
+        if(lm_is_insert(&lm)==true){
+            lm_fetch_ngram(&lm);
+            trie_insert(&db,&lm);
+        }
+        else if(lm_is_delete(&lm)==true){
+            lm_fetch_ngram(&lm);
+            trie_delete(&db,&lm);
+        }
+        else if(lm_is_query(&lm)==true){
+            trie_search(&db,&lm,&rm);
+        }
+        else{
+            fprintf(stderr,"Corrupted line\n");
+        }
+        has_line=lm_fetch_line(&lm);
+    }
+
+    char buffer[100];
+    int size=100;
+    fseek(out, 0, SEEK_SET);
+    if(fgets(buffer, size, out)){
+        int r;
+        r=strcmp("this is a dog\n", buffer);
+        EXPECT_EQ(0, r);
+    }
+
+    trie_fin(&db);
+    result_manager_fin(&rm);
+    line_manager_fin(&lm);
+    fclose(in);
+    fclose(out);
 }
 
-TEST(AddDeleteSearch, TheSameNgram){
 
-}*/
+TEST(AddDeleteSearch, TheSameNgram){
+    line_manager lm;
+    FILE* in, *out;
+    in=fopen("input/TheSameNgram", "r");
+    out=fopen("output/TheSameNgram", "w+");
+    line_manager_init(&lm, in, 'Q');
+    trie db;
+    trie_init(&db,5);
+    result_manager rm;
+    result_manager_init(&rm, out);
+    bool has_line;
+    has_line=lm_fetch_line(&lm);
+    while(has_line==true){
+        if(lm_is_insert(&lm)==true){
+            lm_fetch_ngram(&lm);
+            trie_insert(&db,&lm);
+        }
+        else if(lm_is_delete(&lm)==true){
+            lm_fetch_ngram(&lm);
+            trie_delete(&db,&lm);
+        }
+        else if(lm_is_query(&lm)==true){
+            trie_search(&db,&lm,&rm);
+        }
+        else{
+            fprintf(stderr,"Corrupted line\n");
+        }
+        has_line=lm_fetch_line(&lm);
+    }
+
+    char buffer[100];
+    int size=100;
+    fseek(out, 0, SEEK_SET);
+    if(fgets(buffer, size, out)){
+        int r;
+        r=strcmp("-1\n", buffer);
+        EXPECT_EQ(0, r);
+    }
+
+    trie_fin(&db);
+    result_manager_fin(&rm);
+    line_manager_fin(&lm);
+    fclose(in);
+    fclose(out);
+}
+
 /*
 
 TEST(CompletePhrase, IsANotphrase){
-    ASSERT_EQ(false, complete_phrase("this is a"));
+    EXPECT_EQ(false, complete_phrase("this is a"));
 }
 
 TEST(CompletePhrase, IsAphrase){
-    ASSERT_EQ(true, complete_phrase("this is a\n"));
+    EXPECT_EQ(true, complete_phrase("this is a\n"));
 }
 
 TEST(Fetchline, ALLGOOD){
@@ -122,7 +261,7 @@ TEST(Fetchline, ALLGOOD){
     FILE* in;
     in=fopen("commands.txt","r");
     line_manager_init(&lm, in, 'I');
-    ASSERT_EQ(true,lm_fetch_line(&lm));
+    EXPECT_EQ(true,lm_fetch_line(&lm));
     line_manager_fin(&lm);
     fclose(in);
 }
@@ -132,7 +271,7 @@ TEST(Fetchline, WrongInput){
     FILE* in;
     in=fopen("commands.txt","r");
     line_manager_init(&lm, in, 'Q');
-    ASSERT_EQ(false ,lm_fetch_line(&lm));
+    EXPECT_EQ(false ,lm_fetch_line(&lm));
     line_manager_fin(&lm);
     fclose(in);
 }
@@ -147,7 +286,7 @@ TEST(Fetchline, WrongInput){
     in=fopen("commands.txt","r");
     do the routine for one insertion
     change delete to return true-false and check if true is returned
-    ASSERT_EQ(true, insert(....));
+    EXPECT_EQ(true, insert(....));
 }*/
 
 int main(int argc, char **argv) {
