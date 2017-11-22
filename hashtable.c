ddf980b621300707058ebfaab2c5824ddce7239e
Gdask <gdasky@gmail.com>
2017-11-22 21:38:54 +0200

    redistribute fully implemented

--- hashtable.c
+++ hashtable.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "hashtable.h"
 
 /*djb2, to turn a string to int*/
@@ -37,17 +38,6 @@ void hashtable_init(hashtable* obj, int ca_bucket_size, int bucket_size){
     obj->init_size=ca_bucket_size;
 }
 
-bool hash_append(hashtable* obj, char* word){
-    int key=hash_function(obj, word);
-    
-    loc_res res = ca_locate_bin(&obj->ca_bucket[key], word);
-    bool overflow;
-    if(res.found==false){
-        overflow=ca_bucket_append(&obj->ca_bucket[key], word, res.index);
-    }
-    return overflow;
-}
-
 bool ca_bucket_append(children_arr* obj, char* input_word, int goal_index){
     if(goal_index == obj->Size || obj->First_Available_Slot == obj->Size){
         return false; 
@@ -63,6 +53,29 @@ bool ca_bucket_append(children_arr* obj, char* input_word, int goal_index){
     obj->First_Available_Slot++;
     tn_leaf(&obj->Array[goal_index],input_word);
     return true;
+}
+
+bool hash_append(hashtable* obj, char* word){
+    int key=hash_function(obj, word);
+    
+    loc_res res = ca_locate_bin(&obj->ca_bucket[key], word);
+    bool overflow=false;
+    if(res.found==false){
+        overflow=ca_bucket_append(&obj->ca_bucket[key], word, res.index);
+    }
+    return overflow;
+}
+
+void ca_bucket_delete(children_arr* obj,int goal_index){
+    if(goal_index == obj->First_Available_Slot-1){
+        //tn_fin(&obj->Array[goal_index]);
+        obj->First_Available_Slot--;
+        return;
+    }
+
+    size_t movable =(obj->First_Available_Slot-goal_index-1)*sizeof(trie_node);
+    memmove(&obj->Array[goal_index],&obj->Array[goal_index+1],movable);
+    obj->First_Available_Slot--;
 }
 
 void hashtable_overflow(hashtable* obj){
@@ -98,8 +111,7 @@ void update_round(hashtable* obj){
 void hash_redistribute(hashtable* obj){
     /*take all trie nodes from children array and check if it can be move to new bucket*/
     int key=obj->p;
-    int i=0, end;
-    end=obj->ca_bucket[key].First_Available_Slot;
+    int i=0,movable=0;
 
     while(obj->ca_bucket[key].First_Available_Slot < i){
         char* word=  obj->ca_bucket[key].Array[i].Word;
@@ -107,9 +119,12 @@ void hash_redistribute(hashtable* obj){
         if(key_res!=obj->p){
             //GEORGE, make the move between obj->ca_bucket[key].Array[i] to obj->ca_bucket[obj->size-1]
             obj->ca_bucket[obj->size-1].Array[movable]=obj->ca_bucket[key].Array[i];
-            
+            movable++;
+            ca_bucket_delete(&obj->ca_bucket[key],i);
         }
+        i++;
     }
+    obj->ca_bucket[obj->size-1].First_Available_Slot=movable;
 }
 
 void hashtable_insert(hashtable* obj, char* word){
