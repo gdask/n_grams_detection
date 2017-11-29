3883fb498530ab188790ae509d2f4ad8314ae2a8
eirini.kaklopoulou@gmail.com <eirini.kaklopoulou@gmail.com>
2017-11-29 23:30:09 +0200

    Heap structure is created but not tested

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS = -Wall -g -O3
 
 OBJECTS = trie.o trie_node.o main.o string_utils.o topk.o hashtable.o ./filters/bloom_filter.o ./filters/murmur3.o ./filters/pointer_set.o
 SOURCES = trie.c trie_node.c main.c string_utils.c topk.c hashtable.c ./filters/bloom_filter.c ./filters/murmur3.c ./filters/pointer_set.c
-HEADERS = trie.h trie_node.h string_utils.h topk.h hashtable.h ./filters/bloom_filter.h ./filters/murmur3.h ./filters/pointer_set.h
+HEADERS = trie.h trie_node.h string_utils.h topk.h hashtable.h ./filters/bloom_filter.h ./filters/murmur3.h ./filters/pointer_set.h 
 
 comp : $(OBJECTS)
 	$(CC) -g $(OBJECTS) -o $(OUT)
--- topk.h
+++ topk.h
@@ -2,20 +2,35 @@
 #define	TOP_K_H
 #define INIT_SIZE_QUEUE 512
 #define INIT_SIZE_NGRAM 128
+#define LCHILD(x) 2 * x + 1 // left child
+#define RCHILD(x) 2 * x + 2 // right child
+#define PARENT(x) (x - 1) / 2 // parent
+
+#define INIT_SIZE_HEAP 4
 #include <stdbool.h>
 #include <stdio.h>
 
 typedef struct na_node{
     int rank;
     char* ngram; //initialized to initbufsize
+    bool visited;
     int bufsize;
 }na_node;
+
+typedef struct maxHeap{
+    int size ;
+    int k; //k is needed for topK
+    int FirstAvailableSlot;
+    na_node *elem ;
+}maxHeap ;
 
 typedef struct ngram_array{
         na_node* Array; 
         int bufsize;
         int first_available_slot;
+        maxHeap* heap;
 }ngram_array;
+
 
 void na_init(ngram_array *obj);
 void na_fin(ngram_array *obj);
@@ -38,5 +53,23 @@ void na_lookup(ngram_array *obj, char* input_ngram, int len);
 void quickSort(ngram_array obj[], int low, int high);
 int partition (ngram_array *obj, int low, int high);
 void na_topk_sort(ngram_array* obj, int k);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Heap Functions
+
+void heap_init(maxHeap* obj, int size);
+void heap_reuse(maxHeap *obj);
+void heap_fin(maxHeap* obj);
+
+void insertNode(maxHeap *obj, int data, char* ngram);
+void swap(na_node *n1, na_node *n2);
+void heapify(maxHeap *obj, int i);
+void buildMaxHeap(maxHeap *obj, na_node *arr, int size);
+
+void inorderTraversal(maxHeap *obj, int i);
+
+void heap_display(maxHeap *obj);
+void topK(maxHeap *obj, int start);
+
 
 #endif
